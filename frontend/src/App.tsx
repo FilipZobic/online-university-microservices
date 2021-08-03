80ff6ef5be0d13ae43979dbce9d0d9f6bc05d77f
StevanPataki99 <stevan.pataki.1999@gmail.com>
2021-08-03 23:09:01 +0200

    Refactor project with removed linting and formating they need to be redone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
 }
 
 export default App;
