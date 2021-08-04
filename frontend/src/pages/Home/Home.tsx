b8443fdca325c3d975f810673ab812561abdeb73
StevanPataki99 <stevan.pataki.1999@gmail.com>
2021-08-04 21:29:39 +0200

    Add react-bootstrap to the project

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 class Home extends React.Component {
     render(): JSX.Element {
         return (
             <div>
                 <h1>HOME PAGE</h1>
+                <Button>Hello</Button>
             </div>
         );
     }
