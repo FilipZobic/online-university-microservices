f130846601eeef8003bdee666de92f93363b554b
Filip <56782972+FilipZobic@users.noreply.github.com>
2021-08-05 20:10:49 +0200

    [API] email-service (#4)
    * Create email-service

---
+++ backend/email-service/src/main/java/rs/singidunum/emailservice/dto/EmailMessageDto.java
@@ -0,0 +1,21 @@
+package rs.singidunum.emailservice.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EmailMessageDto implements Serializable {
+
+    private String sender;
+
+    private String recipient;
+
+    private String subject;
+
+    private String body;
+}
